f7dc45263a05abe751760171e26d00b14dbb1eb7
Leonardo Canteros <leo.sebas.065@gmail.com>
2019-11-18 17:52:55 -0300

    Correccion - No contemplaba ejemplo 3
    Bueno toque muchas cosas, el anterior estaba mal asi que lo estoy planteando de manera distinta

--- public_html/js/ejercicio3.js
+++ public_html/js/ejercicio3.js
@@ -8,8 +8,8 @@ function calcCommonChild(){
     result = 0;
     child = "";
     sEnt = s.split(" ");
-    s1 = sEnt[0] ;
-    s2 = sEnt[1];
+    s1 = clearString(sEnt[0],sEnt[1]) ;
+    s2 = clearString(sEnt[1],sEnt[0]);
     auxs2 = s2;
     for(i=0;i < s1.length; i++) {
         letra = s1.charAt(i);
@@ -18,8 +18,34 @@ function calcCommonChild(){
             result++;
             child = child + letra;
             auxs2 = auxs2.substring(i);
+        } else {
+            
         }
     }
     document.querySelector("#result").setAttribute("value",result);
     alert(child);
+}
+/*function clearString(s){
+    sEnt = s.split(" ");
+    s1 = sEnt[0] ;
+    s2 = sEnt[1];
+    s1Vec = s1.split("");
+    s2Vec = s2.split("");
+    for (i=0;i<s1Vec.length();i++){
+        letra = s1Vec[i];
+    }
+*/
+function clearString(s1, s2){
+    s1Vec = s1.split("");
+    s2Vec = s2.split("");
+    var result = [];
+    for (i=0;i<s1Vec.length();i++){
+        letra = s1Vec[i];
+        for (i=0;i<s2Vec.length();i++){
+            if (letra === s2Vec[i]){
+                result.push(letra);
+            }
+        }
+    }
+    return result;
 }
